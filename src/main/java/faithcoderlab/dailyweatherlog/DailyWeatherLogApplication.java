140ec8c7cad6e6eb029323460f27a3fe86017a5f
FaithCoderLab <seinplan97@gmail.com>
2025-03-05 16:11:39 +0900

    initial commit

---
+++ src/main/java/faithcoderlab/dailyweatherlog/DailyWeatherLogApplication.java
@@ -0,0 +1,13 @@
+package faithcoderlab.dailyweatherlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyWeatherLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyWeatherLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/faithcoderlab/dailyweatherlog/DailyWeatherLogApplicationTests.java
@@ -0,0 +1,13 @@
+package faithcoderlab.dailyweatherlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyWeatherLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
